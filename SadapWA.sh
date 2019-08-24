7317a0f84be42d99b86d85e86f457c699df977fa
Jimmi Setiawan <53130905+MiSetya@users.noreply.github.com>
2019-08-24 14:45:13 +0700

    Add files via upload

---
+++ SadapWA.sh
@@ -0,0 +1,241 @@
+#SadapWA
+#Coded by. MiSetya
+#Jangan recode ngentod!!
+#mau recode izin 0823-8623-4828
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo $cy"<|=======================================|>"
+echo $cy "||     $i Sadap WhatsApp Version 1.0     $cy||"
+echo $cy "||     $i Author: MiSetya                $cy||"
+echo $cy "||     $i Team  : Light Cyber Indonesia  $cy||"
+echo $cy"<|=======================================|>"
+echo
+sleep 4
+echo $i"<[===============================]>"
+echo $i"<[  $me 0.Exit This Tool $i           ]>"
+echo $i"<[  $pu 1.Sadap Nomor Telkomsel $i    ]>"
+echo $i"<[  $me 2.Sadap Nomor XL   $i         ]>"
+echo $i"<[  $pu 3.Sadap Nomor Three   $i      ]>"
+echo $i"<[  $me 4.Sadap Nomor Axis    $i      ]>"
+echo $i"<[  $pu 5.Sadap Nomor Smartfren  $i   ]>"
+echo $i"<[===============================]>"
+echo
+echo $bi "╭─"$i"Pilih Salah Satu"
+read -p " ╰─────────$MiSetya>>"  pil
+
+if [ $pil = '1' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MiSetya>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+sleep 4
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+fi
+
+if [ $pil = '2' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MiSetya>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+sleep 4
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+fi
+
+if [ $pil = '3' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MiSetya>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+sleep 4
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+fi
+
+if [ $pil = '4' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MiSetya>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+sleep 4
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+fi
+
+if [ $pil = '5' ]
+then
+echo
+echo
+echo $bi "╭─"$i"Masukan Nomor Target"
+read -p " ╰─────────$MiSetya>>"  pil
+echo
+echo
+echo $i"[•] Mengecek Nomor"
+sleep 5
+echo $bi"Loading...."
+sleep 4
+echo $i"Processing..."
+sleep 7
+echo $bi"10%"
+sleep 8
+echo $i"20%"
+sleep 6
+echo $bi"30%"
+sleep 5
+echo $i"40%"
+sleep 7
+echo $bi"50%"
+sleep 5
+echo $i"60%"
+sleep 9
+echo $bi"70%"
+sleep 4
+echo $i"80%"
+sleep 7
+echo $bi"90%"
+sleep 8
+echo $j"100%"
+sleep 4
+echo $cy"Done!"
+sleep 6
+echo $me"Nomor $pil berhasil di Sadap! Tunggu kode verifikasi"
+fi
+
+if [ $pil = '0' ]
+then
+clear
+echo $cy"Thanks for using this tools"
+sleep 2
+echo $cy"Silahkan menunggu Besok/lusa/selamanya untuk mendapatkan kode verifikasi"
+sleep 1
+echo
+toilet -f future HAHAHAHAH | lolcat
+echo
+echo $me"Contact me kalo mau complain : $i 0823-8623-4828"
+exit
+fi
